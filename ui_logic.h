1c6d8f57096a3277d035aa7e0c12644f3aab6994
DESKTOP-N80N1GI <4b3g0123@office.stust.edu.tw>
2024-12-24 22:38:42 +0800

    sel_judge()-is-condition

--- ui_logic.h
+++ ui_logic.h
@@ -2,5 +2,25 @@
 
 void sel_judge()
 {
-    
+    if(sel == 1)
+    {
+
+    }
+    else if(sel == 2)
+    {
+
+    }
+    else if(sel == 3)
+    {
+
+    }
+    else if(sel != 1 || sel != 2 || sel != 3)
+    {
+        do
+        {
+            
+        } while (sel != 1 && sel != 2 && sel != 3);
+        
+    }
+
 }
